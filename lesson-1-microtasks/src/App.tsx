5a253852a1f32de2db830d07a67c5cf5273815c6
Daria Mochalova <shichimiya74@gmail.com>
2026-03-03 16:30:35 +0500

    useState microtask

--- lesson-1-microtasks/src/App.tsx
+++ lesson-1-microtasks/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import { CarsComponent } from './CarsComponent';
 
@@ -8,10 +9,21 @@ const topCars = [
 ];
 
 function App() {
+    let [a, setA] = useState(1);
+    const onClickHandler = () => {
+        setA(++a);
+    };
+
+    const onClickResetHandler = () => {
+        setA(0);
+    };
     return (
-        <>
+        <div>
             <CarsComponent cars={topCars} />
-        </>
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickResetHandler}>0</button>
+        </div>
     );
 }
 
